33697dc57810e09bac0763306eb811364304958b
yalntue <106251833+yalntue@users.noreply.github.com>
2023-08-02 14:51:23 +0300

    Update main.rs

--- polkadot_task_2/src/main.rs
+++ polkadot_task_2/src/main.rs
@@ -1,38 +1,47 @@
-// Define a struct called FilterCondition with a single field of the desired type for filtering.
-struct FilterCondition<T> {
-    condition: T,
+// Define a struct for filtering conditions.
+struct FilterCondition {
+    value: i32,
+    is_even: bool,
 }
 
-// Implement a method called is_match on the FilterCondition struct that takes a reference to an item of the same type as the filter condition and returns a boolean indicating whether the item matches the condition.
-impl<T: PartialEq> FilterCondition<T> {
-    fn is_match(&self, item: &T) -> bool {
-        item == &self.condition
+// Implement an is_match method on the FilterCondition struct.
+impl FilterCondition {
+    fn is_match(&self, item: &i32) -> bool {
+        if self.is_even {
+            *item % 2 == 0
+        } else {
+            *item % 2 != 0
+        }
     }
 }
 
 // Define a function called custom_filter that takes a collection (e.g., a vector) and a reference to a FilterCondition object as arguments.
 // The function should iterate over the elements in the collection and return a new collection containing only the elements that match the filter condition.
-fn custom_filter<T>(collection: &Vec<T>, filter_condition: &FilterCondition<T>) -> Vec<T>
-where
-    T: PartialEq + Clone,
-{
-    collection
-        .iter()
-        .cloned()
-        .filter(|item| filter_condition.is_match(item))
-        .collect()
+fn custom_filter(collection: &Vec<i32>, condition: &FilterCondition) -> Vec<i32> {
+    let mut filtered_items = Vec::new();
+
+    for item in collection {
+        if condition.is_match(item) {
+            filtered_items.push(*item);
+        }
+    }
+
+    filtered_items
 }
 
 fn main() {
-    // Create a collection (e.g., a vector) with some elements
-    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // Create a collection (e.g., a vector) and add some elements to it
+    let numbers = vec![10, 21, 30, 45, 50, 63, 70, 89, 92, 101];
 
     // Initialize a FilterCondition object with the desired value
-    let filter_condition = FilterCondition { condition: 5 };
+    let filter_condition_even = FilterCondition { value: 0, is_even: true };
+    let filter_condition_odd = FilterCondition { value: 0, is_even: false };
 
     // Call the custom_filter function with the collection and the FilterCondition object, storing the result in a new variable
-    let filtered_numbers = custom_filter(&numbers, &filter_condition);
+    let filtered_even_numbers = custom_filter(&numbers, &filter_condition_even);
+    let filtered_odd_numbers = custom_filter(&numbers, &filter_condition_odd);
 
     // Print the filtered result to the console
-    println!("Filtered Numbers: {:?}", filtered_numbers);
+    println!("Filtered Even Numbers: {:?}", filtered_even_numbers);
+    println!("Filtered Odd Numbers: {:?}", filtered_odd_numbers);
 }
